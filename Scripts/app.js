759316a47cb5cb1be37dd779e37f98c5dc4a210c
Naheed Shaikh <naheed.shaikh@dcmail.ca>
2020-03-07 01:46:09 -0500

    Added Confirm password event

--- Scripts/app.js
+++ Scripts/app.js
@@ -40,142 +40,284 @@ class User
 let app;
 (function(app){
 
-    // Declare Function Variables here...
-    console.log("%cDeclaring Variables", "color: red;")
-    let contactObject = new Contact();
+        // Declare Function Variables here...
+        console.log("%cDeclaring Variables", "color: red;")
+        let contactObject = new Contact();
 
-    /**
-     * Variable initialization in this function
-     *
-     */
-    function Start()
-    {
-       PageSwitcher();
-
-        Main();
-    }
-
-    function PageSwitcher()
-    {
-        let name = window.location.pathname;
-
-       let pageName = name.substring(1, name.length - 5);
-
-       switch(pageName)
+        /**
+         * Variable initialization in this function
+         *
+         */
+        function Start()
         {
-            case "index":
-               DisplayHomePageContent();
-                break;
-            case "products":
-                DisplayProductsContent();
-                break;
-            case "services":
-                DisplayServicesContent();
-                break;
-            case "about":
-                DisplayAboutContent();
-                break;
-            case "contact":
-                DisplayContactContent();
-                break;
-            case "projects":
-                DisplayProjectsContent();
-                break;
-            case "login":
-                DisplayLoginContent();
-                break;
-            case "register":
-                DisplayRegisterContent();
-                break;
-            default:
-                console.error("Landed in a page that is not defined");
-                break;
+        PageSwitcher();
+
+            Main();
         }
 
-        // add a class of active to the active link
-        $("#"+pageName).addClass("active");
-    }
+        function PageSwitcher()
+        {
+            let name = window.location.pathname;
 
-    function DisplayHomePageContent()
-    {
-        document.getElementById("home").className = "nav-item active";
-        /* $("button").click(()=>{
-            location.href = "projects.html";
-        }); */
+        let pageName = name.substring(1, name.length - 5);
 
-        document.title = "WEBD6201 - Home";
+        switch(pageName)
+            {
+                case "index":
+                DisplayHomePageContent();
+                    break;
+                case "products":
+                    DisplayProductsContent();
+                    break;
+                case "services":
+                    DisplayServicesContent();
+                    break;
+                case "about":
+                    DisplayAboutContent();
+                    break;
+                case "contact":
+                    DisplayContactContent();
+                    break;
+                case "projects":
+                    DisplayProjectsContent();
+                    break;
+                case "login":
+                    DisplayLoginContent();
+                    break;
+                case "register":
+                    DisplayRegisterContent();
+                    break;
+                default:
+                    console.error("Landed in a page that is not defined");
+                    break;
+            }
 
-        let progressbar = $( "#progressBar" ).progressbar({
-            value: 37
-          });
+            // add a class of active to the active link
+            $("#"+pageName).addClass("active");
+        }
 
-        console.log(progressbar);
+        function DisplayHomePageContent()
+        {
+            document.getElementById("home").className = "nav-item active";
+            /* $("button").click(()=>{
+                location.href = "projects.html";
+            }); */
 
-        $("#projectsButton").click(function(){
-            $(this).fadeOut(3000, "linear", ()=>{
-                $(this).fadeIn(1000, "linear", ()=>{
-                    location.href = "projects.html";
+            document.title = "WEBD6201 - Home";
+
+            let progressbar = $( "#progressBar" ).progressbar({
+                value: 37
+            });
+
+            console.log(progressbar);
+
+            $("#projectsButton").click(function(){
+                $(this).fadeOut(3000, "linear", ()=>{
+                    $(this).fadeIn(1000, "linear", ()=>{
+                        location.href = "projects.html";
+                    });
                 });
             });
-        });
-    }
-
-    function DisplayProductsContent()
-    {
-        document.title = "WEBD6201 - Products";
-    }
-
-    function DisplayServicesContent()
-    {
-        document.title = "WEBD6201 - Services";
-    }
-
-    function DisplayAboutContent()
-    {
-        document.title = "WEBD6201 - About Us";
-    }
-
-    function DisplayContactContent()
-    {
-        document.title = "WEBD6201 - Contact Us";
-        function clearForm()
-        {
-            //document.getElementById("contactForm").reset();
-            $("#contactForm")[0].reset();
-            $("#errorMessage").hide();
         }
 
-        function validateInput(selector, condition, errorMessage)
+        function DisplayProductsContent()
         {
-            if(condition)
+            document.title = "WEBD6201 - Products";
+        }
+
+        function DisplayServicesContent()
+        {
+            document.title = "WEBD6201 - Services";
+        }
+
+        function DisplayAboutContent()
+        {
+            document.title = "WEBD6201 - About Us";
+        }
+
+        function DisplayContactContent()
+        {
+            document.title = "WEBD6201 - Contact Us";
+            function clearForm()
             {
-                $("#errorMessage").show();
-                $("#errorMessage").text(errorMessage);
-                $(selector).select();
-                $(selector).css("border", "2px solid red");
-            }
-            else
-            {
+                //document.getElementById("contactForm").reset();
+                $("#contactForm")[0].reset();
                 $("#errorMessage").hide();
-                $(selector).css("border", "1px solid #ced4da");
             }
+
+            function validateInput(selector, condition, errorMessage)
+            {
+                if(condition)
+                {
+                    $("#errorMessage").show();
+                    $("#errorMessage").text(errorMessage);
+                    $(selector).select();
+                    $(selector).css("border", "2px solid red");
+                }
+                else
+                {
+                    $("#errorMessage").hide();
+                    $(selector).css("border", "1px solid #ced4da");
+                }
+            }
+
+            $("#errorMessage").hide();
+            $("#contactName").select();
+
+            // Contact Name Events
+            $("#contactName").blur((e)=>
+            {
+                validateInput("#contactName",( $("#contactName").val().length < 2),"Contact Name is Too Short");
+            });
+
+            $("#contactName").focus((e)=>
+            {
+                $("#contactName").select();
+            });
+
+            // Email Events
+            $("#emailAddress").blur((e)=>
+            {
+                validateInput("#emailAddress",($("#emailAddress").val().length < 8) || (!$("#emailAddress").val().includes("@")),"Invalid Email Address");
+            });
+
+            $("#emailAddress").focus((e)=>
+            {
+                $("#emailAddress").select();
+            });
+
+            // Contact Number Events
+            $("#contactNumber").blur((e)=>
+            {
+                let phonePattern = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/
+                let phoneNumber = $("#contactNumber").val();
+
+                validateInput("#contactNumber",( !phonePattern.test(phoneNumber)),"Invalid Contact Number");
+            });
+
+            $("#contactNumber").focus((e)=>
+            {
+                $("#contactNumber").select();
+            });
+
+            // Contact Message Events
+            $("#contactMessage").blur((e)=>
+            {
+                validateInput("#contactMessage",( $("#contactMessage").val().length < 2 ),"Contact Message Too Short");
+            });
+
+            $("#contactMessage").focus((e)=>
+            {
+                $("#contactMessage").select();
+            });
+
+
+            $("#contactForm").submit  ((e)=>
+            {
+                if(document.getElementById("contactForm").checkValidity() == false)
+                {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    console.log("form not valid");
+                }
+
+                
+                let contactName = $("#contactName").val();
+                let emailAddress = $("#emailAddress").val();
+                let contactNumber = $("#contactNumber").val();
+                let contactMessage = $("#contactMessage").val();
+
+                console.log(`Contact Name: ${contactName}`);
+                console.log(`Email Address: ${emailAddress}`);
+                console.log(`Contact Number: ${contactNumber}`);
+                console.log(`Contact Message: ${contactMessage}`);
+
+                contactObject.contactName = contactName;
+                contactObject.emailAddress = emailAddress;
+                contactObject.contactNumber = contactNumber;
+                contactObject.contactMessage = contactMessage;
+
+                console.log(contactObject);
+
+                clearForm();
+            });
+
+            $("#resetButton").click((e)=>
+            {
+                e.preventDefault();
+                if(confirm("Are You Sure?"))
+                {
+                    clearForm();
+                }
+
+                
+            });
         }
 
-        $("#errorMessage").hide();
-        $("#contactName").select();
-
-        // Contact Name Events
-        $("#contactName").blur((e)=>
+        function DisplayProjectsContent()
         {
-            validateInput("#contactName",( $("#contactName").val().length < 2),"Contact Name is Too Short");
+            document.title = "WEBD6201 - Projects";
+        }
+
+        /************** Login Page **************/
+        
+        function DisplayLoginContent()
+        {
+            document.title = "WEBD6201 - Login";
+
+            $("#loginForm").submit  ((e)=>
+            {
+            
+                //userName show at the nevbar after contact us element
+                let userName = $('input#contactName').val();
+
+                
+                $("#login").before ($("<li>").addClass("navbar-text").text('Welcome '+ userName)).show();
+                console.log(userName);
+
+                e.preventDefault();
+                e.stopPropagation();
+                $("#loginForm")[0].reset();
+                $("#login").hide();
+                $("#logout").show();
+
+            });
+
+        }
+        /************** Register Page **************/
+        function DisplayRegisterContent()
+        {
+            document.title = "WEBD6201 - Register";
+
+            let errorMessage = $("<div>").attr("id", "errorMessage").addClass("alert alert-danger");
+            $(".hint-text").append(errorMessage);
+
+            $("#errorMessage").hide();
+            $("#firstName").select();
+
+            // First Name Event
+        $("#firstName").blur((e)=>
+        {
+            validateInput("#firstName",( $("#firstName").val().length < 2),"First Name is too short.");
         });
 
-        $("#contactName").focus((e)=>
+        $("#firstName").focus((e)=>
         {
-            $("#contactName").select();
+            $("#firstName").select();
         });
 
-        // Email Events
+        // Last Name Event
+        $("#lastName").blur((e)=>
+        {
+            validateInput("#lastName",( $("#lastName").val().length < 2),"Last Nameis too short.");
+        });
+
+        $("#lastName").focus((e)=>
+        {
+            $("#lastName").select();
+        });
+
+        // Email Event
         $("#emailAddress").blur((e)=>
         {
             validateInput("#emailAddress",($("#emailAddress").val().length < 8) || (!$("#emailAddress").val().includes("@")),"Invalid Email Address");
@@ -186,169 +328,40 @@ let app;
             $("#emailAddress").select();
         });
 
-        // Contact Number Events
-        $("#contactNumber").blur((e)=>
+        // Username Events
+        $("#username").blur((e)=>
         {
-            let phonePattern = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/
-            let phoneNumber = $("#contactNumber").val();
-
-            validateInput("#contactNumber",( !phonePattern.test(phoneNumber)),"Invalid Contact Number");
+            validateInput("#username",( $("#username").val().length < 2),"User name must not be less then 2 characters.");
         });
 
-        $("#contactNumber").focus((e)=>
+        $("#userame").focus((e)=>
         {
-            $("#contactNumber").select();
+            $("#userame").select();
         });
 
-        // Contact Message Events
-        $("#contactMessage").blur((e)=>
+        // Password Events
+        $("#password").blur((e)=>
         {
-            validateInput("#contactMessage",( $("#contactMessage").val().length < 2 ),"Contact Message Too Short");
+            validateInput("#password",($("#password").val().length < 2),"Password must be atleast 6 characters in length.");
         });
 
-        $("#contactMessage").focus((e)=>
+        $("#password").focus((e)=>
         {
-            $("#contactMessage").select();
+            $("#password").select();
         });
 
-
-        $("#contactForm").submit  ((e)=>
+        // Confirm Password Events
+        $("#confirmPassword").blur((e)=>
         {
-            if(document.getElementById("contactForm").checkValidity() == false)
-            {
-                e.preventDefault();
-                e.stopPropagation();
-                console.log("form not valid");
-            }
-
-            
-            let contactName = $("#contactName").val();
-            let emailAddress = $("#emailAddress").val();
-            let contactNumber = $("#contactNumber").val();
-            let contactMessage = $("#contactMessage").val();
-
-            console.log(`Contact Name: ${contactName}`);
-            console.log(`Email Address: ${emailAddress}`);
-            console.log(`Contact Number: ${contactNumber}`);
-            console.log(`Contact Message: ${contactMessage}`);
-
-            contactObject.contactName = contactName;
-            contactObject.emailAddress = emailAddress;
-            contactObject.contactNumber = contactNumber;
-            contactObject.contactMessage = contactMessage;
-
-            console.log(contactObject);
-
-            clearForm();
+            let userPassword = $("#confirmPassword").val();
+            let confirmUserPass = $("#password").val();
+            validateInput("#confirmPassword",(confirmUserPass != userPassword),"Confirm Password must match the Password.");
         });
-
-        $("#resetButton").click((e)=>
-        {
-            e.preventDefault();
-            if(confirm("Are You Sure?"))
-            {
-                clearForm();
-            }
-
-            
-        });
-    }
-
-    function DisplayProjectsContent()
-    {
-        document.title = "WEBD6201 - Projects";
-    }
-
-    /************** Login Page **************/
     
-    function DisplayLoginContent()
-    {
-        document.title = "WEBD6201 - Login";
-
-        $("#loginForm").submit  ((e)=>
+        $("#confirmPassword").focus((e)=>
         {
-           
-            //userName show at the nevbar after contact us element
-            let userName = $('input#contactName').val();
-
-             
-            $("#login").before ($("<li>").addClass("navbar-text").text('Welcome '+ userName)).show();
-            console.log(userName);
-
-            e.preventDefault();
-            e.stopPropagation();
-            $("#loginForm")[0].reset();
-            $("#login").hide();
-            $("#logout").show();
-
+            $("#confirmPassword").select();
         });
-
-    }
-    /************** Register Page **************/
-    function DisplayRegisterContent()
-    {
-        document.title = "WEBD6201 - Register";
-
-        let errorMessage = $("<div>").attr("id", "errorMessage").addClass("alert alert-danger");
-        $(".hint-text").append(errorMessage);
-
-        $("#errorMessage").hide();
-        $("#firstName").select();
-
-        // First Name Event
-    $("#firstName").blur((e)=>
-    {
-        validateInput("#firstName",( $("#firstName").val().length < 2),"First Name is too short.");
-    });
-
-    $("#firstName").focus((e)=>
-    {
-        $("#firstName").select();
-    });
-
-    // Last Name Event
-    $("#lastName").blur((e)=>
-    {
-        validateInput("#lastName",( $("#lastName").val().length < 2),"Last Nameis too short.");
-    });
-
-    $("#lastName").focus((e)=>
-    {
-        $("#lastName").select();
-    });
-
-    // Email Event
-    $("#emailAddress").blur((e)=>
-    {
-        validateInput("#emailAddress",($("#emailAddress").val().length < 8) || (!$("#emailAddress").val().includes("@")),"Invalid Email Address");
-    });
-
-    $("#emailAddress").focus((e)=>
-    {
-        $("#emailAddress").select();
-    });
-
-    // Username Events
-    $("#username").blur((e)=>
-    {
-        validateInput("#username",( $("#username").val().length < 2),"User name must not be less then 2 characters.");
-    });
-
-    $("#userame").focus((e)=>
-    {
-        $("#userame").select();
-    });
-
-    // Password Events
-    $("#password").blur((e)=>
-    {
-        validateInput("#password",($("#password").val().length < 2),"Password must be atleast 6 characters in length.");
-    });
-
-    $("#password").focus((e)=>
-    {
-        $("#password").select();
-    });
     }
 
     /**
